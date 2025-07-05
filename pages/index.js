6183ea93a15d7653ce73c5e80882e92614607e94
ABHRCC <info@abhrc.org>
2025-07-06 01:43:20 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ABHRC – Home</title>
+      </Head>
+      <main style={{ padding: 30 }}>
+        <h1>Welcome to ABHRC</h1>
+        <p>This is the homepage of the American Business & Human Rights Consultation Center.</p>
+      </main>
+    </>
+  );
+}
