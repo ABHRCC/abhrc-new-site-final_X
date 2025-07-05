066a9577fca8da90452b30e627a570fe31ed7595
ABHRCC <info@abhrc.org>
2025-07-06 01:03:04 +0300

    Update human-rights-guidance.js

--- pages/courses/human-rights-guidance.js
+++ pages/courses/human-rights-guidance.js
@@ -6,7 +6,7 @@ export default function HumanRightsGuidance() {
       <Head>
         <title>Human Rights Guidance</title>
       </Head>
-      <main style={ padding: 30 }>
+      <main style={{ padding: 30 }}>
         <h1>Human Rights Guidance</h1>
         <p>This is a detailed page for the "Human Rights Guidance" course offered by ABHRC.</p>
       </main>
