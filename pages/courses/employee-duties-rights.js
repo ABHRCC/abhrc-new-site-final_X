e05c2eb4391dc62c7855eab1b4fbbe3d7d43126a
ABHRCC <info@abhrc.org>
2025-07-06 01:03:24 +0300

    Update employee-duties-rights.js

--- pages/courses/employee-duties-rights.js
+++ pages/courses/employee-duties-rights.js
@@ -6,7 +6,7 @@ export default function EmployeeDutiesRights() {
       <Head>
         <title>Employee Duties Rights</title>
       </Head>
-      <main style={ padding: 30 }>
+      <main style={{ padding: 30 }}>
         <h1>Employee Duties Rights</h1>
         <p>This is a detailed page for the "Employee Duties Rights" course offered by ABHRC.</p>
       </main>
