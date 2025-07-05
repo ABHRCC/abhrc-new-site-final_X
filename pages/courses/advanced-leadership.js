a08d13d1b8304b1d65c9a734976be5239446f66c
ABHRCC <info@abhrc.org>
2025-07-06 01:04:17 +0300

    Update advanced-leadership.js

--- pages/courses/advanced-leadership.js
+++ pages/courses/advanced-leadership.js
@@ -6,7 +6,7 @@ export default function AdvancedLeadership() {
       <Head>
         <title>Advanced Leadership</title>
       </Head>
-      <main style={ padding: 30 }>
+      <main style={{ padding: 30 }}>
         <h1>Advanced Leadership</h1>
         <p>This is a detailed page for the "Advanced Leadership" course offered by ABHRC.</p>
       </main>
