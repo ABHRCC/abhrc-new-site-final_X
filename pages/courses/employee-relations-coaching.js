28bca175523d89d0b3dc05962082e87da9ea6707
ABHRCC <info@abhrc.org>
2025-07-06 00:36:28 +0300

    Update employee-relations-coaching.js

--- pages/courses/employee-relations-coaching.js
+++ pages/courses/employee-relations-coaching.js
@@ -6,7 +6,7 @@ export default function EmployeeRelationsCoaching() {
       <Head>
         <title>Employee Relations Coaching</title>
       </Head>
-      <main style={ padding: 30 }>
+      <main style={{ padding: 30 }}>
         <h1>Employee Relations Coaching</h1>
         <p>This is a detailed page for the "Employee Relations Coaching" course offered by ABHRC.</p>
       </main>
