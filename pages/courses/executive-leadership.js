09260de25afc69ea5a623632cf7130e3321db244
ABHRCC <info@abhrc.org>
2025-07-06 01:01:09 +0300

    Update executive-leadership.js

--- pages/courses/executive-leadership.js
+++ pages/courses/executive-leadership.js
@@ -6,7 +6,7 @@ export default function ExecutiveLeadership() {
       <Head>
         <title>Executive Leadership</title>
       </Head>
-      <main style={ padding: 30 }>
+      <main style={{ padding: 30 }}>
         <h1>Executive Leadership</h1>
         <p>This is a detailed page for the "Executive Leadership" course offered by ABHRC.</p>
       </main>
