965d9c849748308bb197a71e133a2798378c9e99
ABHRCC <info@abhrc.org>
2025-07-06 01:07:24 +0300

    Update operational-analysis.js

--- pages/services/operational-analysis.js
+++ pages/services/operational-analysis.js
@@ -6,7 +6,7 @@ export default function OperationalAnalysis() {
       <Head>
         <title>Operational Analysis</title>
       </Head>
-      <main style={ padding: 30 }>
+      <main style={{ padding: 30 }}>
         <h1>Operational Analysis</h1>
         <p>This is a detailed page for the "Operational Analysis" service offered by ABHRC.</p>
       </main>
